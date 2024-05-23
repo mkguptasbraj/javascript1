2887a6adc064cb23d436e5fb81f9d9065f3f1a50
mkguptasbraj <mkguptasbraj@gmail.com>
2024-05-23 12:46:31 +0530

    1

--- first.js
+++ first.js
@@ -1,2 +1,53 @@
-console.log("welcome to your college bhu !!");
-console.log("Monu gupta");
+name = "tony stark";
+age = 24;
+x=null
+y = undefined;  
+console.log(null);
+console.log(name);
+console.log("monu gupta");
+console.log("monu gupta");
+var date = 2000;
+console.log(date);
+console.log(age);
+var date = 2001;
+var date = 39999;
+console.log(date);
+console.log(name);
+let fullName= "tony stark"
+let a;
+let b = null;
+let z = BigInt("123454543121");
+let w= Symbol()
+const student = {
+    fullName : "Monu gupta",
+    age:20,
+    cgpa:8.2,
+    ispass: true,
+};
+
+
+
+
+
+
+
+
+
+const product  = {
+    penName:"parker jotter pen",
+    price:270,
+    offer:5,
+    rating:4
+
+};
+
+const instagram = {
+    name:"mkguptasbraj",
+    isfollow : true ,
+    posts:195,
+    follow:'569k',
+    follwing:4,
+    tag:'@mkguptasbraj',
+    Enterpreneur:"Apna College | Ex- Microsoft ,DRDO ",
+    goal:"To educate someone in thr highest privillege "
+};
