d051f6ab6e2989244e16e8700ce926ed4de78aa0
mkguptasbraj <mkguptasbraj@gmail.com>
2024-05-24 17:37:30 +0530

    3

---
+++ lecture3.js
@@ -0,0 +1,10 @@
+console.log("monuaaaa");
+for(let i = 1;i<=100;i++){
+    console.log("Apna college"); // 5 execurte
+
+}
+let sum = 0;
+for(let i = 0;i<=100;i++){
+    sum=  sum +i;}
+    
+console.log(sum);
---
+++ operators.js
@@ -0,0 +1,96 @@
+ /*age=190;
+ console.log(age);
+ let name = 'monu gupta';
+ name = 'sonu gupta';
+ console.log(name);*/
+ //console .log('monu gupta');
+//  let a= 10;
+ //let b= 20;
+ //console.log("a + b = " ,a+b);
+ //console.log("a - b = " ,b-a);
+ //console.log("a * b =",a*b );
+ //console.log("a /b =" ,a/b);
+//  console.log("a **b =" ,a**b );
+/*
+let a= 10;
+let b = 20;
+let c= "10"
+console.log(a===c);
+console.log(a!=c);
+console.log(a==c);
+console.log(a==b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a>b);
+// console.log(a<b);
+// */
+/* let age =  25;
+ if(age>18){
+    console.log("you can vote");
+ }
+ if(age<18){
+    console.log("you cannot vote");
+//  }*/
+/*
+ let mode="dark";
+ let color;
+ if (mode== "dark"){
+    color="black";
+ }
+ else {
+ color="white"
+ }
+console.log(color);*/
+/*
+ let number = 10;
+ if(number%2==0){
+  console.log("even number");
+
+ }
+ else{
+    console.log("odd number");
+ }
+ */
+ /* let age = 25;
+ //let result = age >=18? "adult":"not adult";
+// console.log(result);
+ age>=18? console.log("adult"):console.log("not adult");
+*/
+ 
+
+ // let name = prompt("hello");
+ // console.log(name);
+ /* let age = prompt("age");
+  console.log(age);
+  let number = prompt("number");
+  console.log(number);
+  if(number%5===0){
+    console.log("divided");
+  }
+  else{
+console.log("not divided");
+  }
+*/
+let score = prompt("score");
+console .log(score);
+if(score>=90){
+    console.log("you got A grade");
+}
+else if( 80<score<=90)
+{
+    console.log("you got B grade");
+}
+else if(70 < score <=80)
+    {
+        console.log("you got c grade");
+
+    }
+    else if (60 < score <=70){
+        console.log("you got E grade");
+
+    }
+    else{
+        console.log("fail");
+    }
+
+
