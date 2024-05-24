5840b6de2d92c79e06ceb02a2b9f3eb5e2b7d596
mkguptasbraj <mkguptasbraj@gmail.com>
2024-05-25 00:07:40 +0530

    1

--- lecture3.js
+++ lecture3.js
@@ -1,4 +1,4 @@
-console.log("monuaaaa");
+/*console.log("monuaaaa");
 for(let i = 1;i<=100;i++){
     console.log("Apna college"); // 5 execurte
 
@@ -7,4 +7,112 @@ let sum = 0;
 for(let i = 0;i<=100;i++){
     sum=  sum +i;}
     
-console.log(sum);
+console.log("sum of total is",sum);
+*/
+/*let i = 0;
+while(i<10){
+    i++;
+    console.log(i);
+}
+let j = 10;
+do{
+    console.log("apna college");
+    i++;
+
+}
+while(i<=20)
+    */
+
+
+/*let str = "APNACOLLEGE";
+let size = 0;
+for(let i of str){
+    console.log("i =", i);
+    size++;
+}
+console.log("string size=",size); */
+/*let student = {
+  name :"Monu gupta",
+    age:20,
+    cgpa:7.5,
+    ispass:true,
+};
+for(let key in student){
+    console.log("key ==",key,"value",student[key]);
+}
+*/
+/*
+let even = 0;
+for(let i = 0; i<=100;i++){
+   if( i%2==0){
+    console.log("num = ",i);
+    even= even+i;
+    }}
+console.log(even);
+// */
+// let gamenum=25;
+// let usernum=prompt("Guess the game number:");
+// while(usernum !=gamenum){
+//     usernum = prompt("you entered wrong number.guess name:");
+
+// }
+// console.log("congratulations, you entered the right number");
+
+let str = "apnacollege";
+console.log(str.length);
+console.log(str[8]);
+
+
+let obj = {
+item :"pen",
+price:10,
+};
+//console.log(`the cost of`, obj.item ,obj.price);
+//console.log(typeof specialString);
+console.log("The cost of ,${obj.item} ${obj.price} rupees");
+//console.log("apna \n college");
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+
+
+
+
+
+
+
+ let name = prompt("enter the name");
+let username="@" + name.concat()+name.length;
+console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
